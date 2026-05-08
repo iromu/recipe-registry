3255c1265464dd3f93b4ddcc8308a64e02db1de5
Iván Rodríguez Murillo <wantez@gmail.com>
2026-05-08 21:04:40 +0100

    qwen3.6-35b-a3b-fp8-mtp-vllm updated with mods/fix-qwen3.6-chat-template

--- bench.sh
+++ bench.sh
@@ -1,19 +1,20 @@
 MODEL_TAG="Qwen3.6-35B-A3B"
-MODEL_RECIPE="qwen3.6-35b-a3b-fp8-mtp-atlas"
+MODEL_RECIPE="qwen3.6-35b-a3b-fp8-mtp-vllm"
 MODEL_PORT=4000
 
-uvx tool-eval-bench --perf --model ${MODEL_TAG} --backend vllm  \
+uvx tool-eval-bench --perf --model ${MODEL_TAG} --backend litellm  \
   --base-url http://spark.local:${MODEL_PORT}
 
 latest_file=$(find runs -name '*.md' -type f | sort | tail -n1)
 ln -sfn "../$latest_file" "benchmarks/tool_${MODEL_RECIPE}.md"
 
-#uvx llama-benchy \
-#  --base-url http://spark.local:${MODEL_PORT}/v1 \
-#  --model $MODEL_TAG \
-#  --depth 0 4096 8192 \
-#  --pp 2048 \
-#  --tg 128 \
-#  --enable-prefix-caching \
-#  --concurrency 1 2 4 \
-#  --save-result "benchmarks/benchy_${MODEL_RECIPE}.md"
+uvx llama-benchy \
+  --base-url http://spark.local:${MODEL_PORT}/v1 \
+  --model $MODEL_TAG \
+  --depth 0 4096 8192 \
+  --pp 2048 \
+  --tg 128 \
+  --enable-prefix-caching \
+  --concurrency 1 2 4 \
+  --save-result "benchmarks/benchy_${MODEL_RECIPE}.md" \
+  --api-key=$OPENAI_API_KEY
